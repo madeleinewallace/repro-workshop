f3420002c96ace3c26a5a8c082ca44574feb67df
Madeleine Wallace <madeleinewallace98@gmail.com>
2023-09-12 12:17:38 -0700

    Initial commit

---
+++ scripts/01-wrangle.R
@@ -0,0 +1,28 @@
+# This script reads and wrangles data for prepare for analysis
+# Maddie Wallace
+# 2023-09-05
+
+
+# Load packages -----------------------------------------------------------
+# install.packages("readr")
+# install.packages("lme4")
+library(readr)
+library(lme4)
+library(dplyr)
+
+
+# Read data ---------------------------------------------------------------
+## Read the csv file----
+gapminder <- read.csv("data/gapminder_data.csv")
+head(gapminder)
+str(gapminder)
+
+
+# Wrangle data ------------------------------------------------------------
+gap_1980s <- filter(gapminder, year >= 1980, year < 1990)
+gap_big <- filter(gapminder, pop > 100000000000)
+
+
+# Write data --------------------------------------------------------------
+write_csv(gap_1980s, "data_clean/gapminder_1980s.csv")
+write_rds(gap_1980s, "data_clean/gapminder_1980s.rds")
---
+++ scripts/workflow.R
@@ -0,0 +1,10 @@
+# Master document
+
+# First we read and wrangle the data to get just the 1980s----
+source(here::here('R/01-wrangle.R'))
+
+# Then we run a mixed effects model----
+source("R/02-analyze.R")
+
+
+
