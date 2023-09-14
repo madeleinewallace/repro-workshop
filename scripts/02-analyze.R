cb1466048e2fd3105881e2e2f1a6b4220e3da872
Madeleine Wallace <madeleinewallace98@gmail.com>
2023-09-14 12:40:44 -0700

    Creating a new graph test.

--- scripts/02-analyze.R
+++ scripts/02-analyze.R
@@ -12,4 +12,6 @@ gap_1980s <- read_csv("data_clean/gapminder_1980s.csv")
 
 # read data ---------------------------------------------------------------
 
-
+install.packages("ggsave")
+library(ggsave)
+ggsave("mtcars.png")
